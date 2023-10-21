e27677734dc3a4f3e747f855104a2531358bae84
AMAR DESAI <amar@amars-mbp.home>
2023-10-21 22:04:49 +0100

    task controller fix view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,11 @@
 class TasksController < ApplicationController
-    before_action :authenticate_user!
-  
-    def index
-      if params[:show_all] == 'true'
-        @tasks = Task.all
-      else
-        @tasks = current_user.tasks
-      end
+  before_action :authenticate_user!
+
+  def index
+    if params[:show_all] == 'true'
+      @tasks = Task.all
+    else
+      @tasks = current_user.tasks
     end
   end
-  
+end
