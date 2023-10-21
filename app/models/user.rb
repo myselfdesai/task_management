d10aa2634696bbfab0c5f19d9a351fc559127583
AMAR DESAI <amar@amars-mbp.home>
2023-10-21 22:27:36 +0100

    added extra paremeter in user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  # Add the new columns to the permitted parameters
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:location, :latitude, :longitude])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:location, :latitude, :longitude])
+  end 
+  
 end
